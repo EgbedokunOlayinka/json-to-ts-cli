5c256cd43732e09e521e9a2ec6ccc5062a382016
EgbedokunOlayinka <egbedokunolayinka@gmail.com>
2023-06-24 21:46:02 +0100

    make changes to enable typescript run locally

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-import inquirer from "inquirer";
+#!/usr/bin/env node
 
-console.log("setup");
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const log = console.log;
+
+function init() {
+  log("Welcome to json-to-ts");
+  log(chalk.blue("Hello") + " World" + chalk.red("!"));
+}
+
+console.clear();
+init();
