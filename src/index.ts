904cd3df75ce2314c2348b9eae00d7ca85f1b5aa
EgbedokunOlayinka <egbedokunolayinka@gmail.com>
2023-06-25 00:17:43 +0100

    get json from user by console paste or file path

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,70 @@
 
 import inquirer from "inquirer";
 import chalk from "chalk";
+import { readFile } from "fs/promises";
 
 const log = console.log;
+const sleep = (ms = 1000) => new Promise((resolve) => setTimeout(resolve, ms));
 
-function init() {
-  log("Welcome to json-to-ts");
-  log(chalk.blue("Hello") + " World" + chalk.red("!"));
+enum SOURCE_ENUM {
+  FILE = "In a file",
+  PASTE = "Paste it here",
+}
+
+function validateString(value: string) {
+  if (typeof value === "number") value = `${value}`;
+  if (typeof value !== "string" || value.trim() === "") {
+    return "Please enter a string value";
+  }
+  return true;
+}
+
+async function init() {
+  log(chalk.blue("Welcome to json-to-ts"));
+  await sleep(500);
+  askForSource();
+}
+
+async function askForSource() {
+  const answers = await inquirer.prompt({
+    name: "json_source",
+    type: "list",
+    message: "Is your JSON data in a file or will you like to paste it here?",
+    choices: [SOURCE_ENUM.FILE, SOURCE_ENUM.PASTE],
+  });
+
+  if (answers.json_source === SOURCE_ENUM.FILE) {
+    return getJsonFromFileSource();
+  }
+  if (answers.json_source === SOURCE_ENUM.PASTE) {
+    return getJsonFromConsolePaste();
+  }
+}
+
+async function getJsonFromFileSource() {
+  const answers = await inquirer.prompt({
+    name: "json_file_source",
+    type: "input",
+    message: "Please input the path to your JSON file:",
+    validate: (val) => validateString(val),
+  });
+
+  const fileSrc = `${process.cwd()}/${answers.json_file_source}`;
+  const fileContent = await readFile(fileSrc, "utf-8");
+  const formatted = JSON.parse(fileContent);
+  console.log(formatted);
+}
+
+async function getJsonFromConsolePaste() {
+  const answers = await inquirer.prompt({
+    name: "json_console_paste",
+    type: "input",
+    message: "Please paste your JSON data here:",
+    validate: (val) => validateString(val),
+  });
+
+  // const jsonContent = JSON.parse(answers.json_console_paste);
+  console.log(answers.json_console_paste);
 }
 
 console.clear();
